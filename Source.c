b8e6402b6ef9dc9e541c6ce910e00da76cade1ad
lyoum <lyoum97@gmail.com>
2016-11-30 20:08:25 +0800

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,23 +1,79 @@
 #include<stdio.h>
 #include<conio.h>
-#include<string.h>
+#include<string.h>1
 
-//Below function validate whitespace when scanf
-validw(char c[100]){
-	int i;
-	for (i = 0; i < 100; i++){
-		fgets(c, 100, stdin);
-		if (fgets(c, 100, stdin) == NULL)
-			break; break;
+struct userdetail{
+	char uname[50], pwd[50];
+}b;
+
+int recsize = sizeof(b);
+
+int main(){
+	int select;
+	system("cls");
+	printf("\t\tWelcome to Train Reservation System\n");
+	printf("1\tUser\n");
+	printf("2\tAdmin\n");
+	printf("Enter your input: ");
+	scanf("%d", &select);
+	switch(select){
+	case 1:
+		user();
+		break;
+	case 2:
+		adminauthen();
+		break;
+	default:
+		printf("Invalid input. Please try again.");
+		main();
 	}
-	return c;
 }
-hidepass(){
-	char buffer[256] = { 0 };
-	char password[] = "password";
-	char c;
+int user(){
+	int select;
+	system("cls");
+	printf("\t\tUser\n");
+	printf("1\tSign up\n");
+	printf("2\tSign in\n");
+	printf("3\tBack\n");
+	printf("Enter your choice: ");
+	scanf("%d", &select);
+	switch (select){
+	case 1:
+		system("cls");
+		signup();
+		break;
+	case 2:
+		system("cls");
+		signin();
+		break;
+	case 3:
+		system("cls");
+		main();
+		break;
+	}
+}
+signup(){
+	FILE *ptr;
+	int select;
+
+	ptr = fopen("database.txt", "a");
+	if (ptr == NULL)
+	{
+		ptr = fopen("database.txt", "w+");
+		if (ptr == NULL)
+		{
+			printf("Error in opening file : \n");
+			return -1;
+		}
+	}
+	printf("\t\tUser Sign Up\n");
+	printf("Username: ");
+	fflush(stdin);
+	scanf("%[^\n]%*c", b.uname);
+	printf("Password: ");
+	//Starting hide password function
+	char buffer[256] = { 0 }, c;
 	int pos = 0;
-	printf("%s", "Enter password: ");
 	do{
 		c = _getch();
 		if (isprint(c)){
@@ -29,118 +85,186 @@ hidepass(){
 			printf("%s", "\b \b");
 		}
 	} while (c != '\r');
-	if (!strcmp(buffer, password))
-		printf("\n%s\n", "Logged in successfully!");
-	else
-		printf("\n%s\n", "Incorrect login!");
-}
-//User related functions
-int user(){
-	int j;
-	printf("\tUser\n-------------------\n");
-	printf("1\tSign up to register your account\n2\tSign in your account\nSelect your choice: ");
-	scanf_s("%d", &j, 1);
-	if (j == 1){
-		signup();
-	}
-	else if (j == 2){
-		signin();
-	}
-}
-struct userdetail{
-	char name1[100], email[30], gender[10],username[50],pwd[50];
-	int age;
-	unsigned long long int ic, hpno;
-}b;
-int signup(){
-	system("cls");
-	struct userdetail b;
-	int confirm,num;
-	char c[100];
-	FILE *fptr;
-	fptr = fopen("Cprogram.txt", "a+");
-	if (fptr != NULL) {
-		printf("\tSign up to reserve Train(s).\nPlease follow instructions to fill in your details:\n\nFull name: ");
-		validw(b.name1);
-		printf("\nGender (Male/Female): ");
-		scanf_s("%s", b.gender, 10);
-		printf("\nAge: ");
-		scanf_s("%d", &b.age, 10);
-		printf("\nIC number or Passport number: ");
-		scanf("%llu", &b.ic);
-		printf("\nEmail: ");
-		scanf_s("%s", b.email, 30);
-		printf("\nTelephone number: ");
-		scanf("%llu", &b.hpno);
-		printf("\nUsername: ");
-		scanf_s("%s", b.username, 50);
-		printf("\nPassword: ");
-		scanf_s("%s", b.pwd, 50);
+	strcpy(b.pwd, buffer);
+	printf("\nConfirm?\n1\tYes\n2\tNo\n\nEnter your choice: ");
+	scanf("%d", &select);
+	if (select == 1){
 		system("cls");
-		sprintf(c,"Full name: %sGender: %s\nAge: %d\nIC number or Passport number: %llu\nEmail: %s\nTelephone number: %011llu\nUsername: %s\nPassword: %s\nConfirm\?\n\n1\tConfirm\n2\tCancel\nEnter your input: ",
-			b.name1, b.gender, b.age, b.ic, b.email, b.hpno,b.username,b.pwd);
-		printf("%s", c);
-		scanf_s("%d", &confirm, 1);
-		if (confirm == 1){
-			system("cls");
-			fprintf(fptr, "Full name: %sGender: %s\nAge: %d\nIC number or Passport number: %llu\nEmail: %s\nTelephone number: %011llu\nUsername: %s\nPassword: %s\n---------------------------------------------------------------------------------------\n",
-				b.name1, b.gender, b.age, b.ic, b.email, b.hpno,b.username,b.pwd);
-			fclose(fptr);
-			printf("Confirmed. Sign in at main menu to proceed at reservation...\n\n");
-			return main();
-		}
-		else if (confirm == 2){
-			fclose(fptr);
-			system("cls");
-			printf("Cancelled. Returning to main menu...\n\n");
-			return main();
-		}
-		else{
-			system("cls");
-			printf("Invalid input. Returning to main menu...\n\n");
-			return main();
-		}
+		fwrite(&b, recsize, 1, ptr);
+		fflush(stdin);
+		fclose(ptr);
+		printf("Registration confirmed!!\n\nPress any key to continue...\n");
+		getch();
+		user();
 	}
-};
- signin(){
-	 printf("\tSign In\n-------------------\nEnter user credentials\n\n");
-	 printf("Username: ");
- }
- //Admin related functions
-int admin() {
-	int i;
-	char name1[50];
-	printf("Hi. \nEnter something: ");
-	for (i = 0; i < 100; i++){
-		fgets(name1, 100, stdin);
-		if (fgets(name1, 100, stdin) == NULL){
+	else
+		user();
+}
+//Will do later...
+
+signin(){
+	FILE *ptr;
+	int select, flag = 0, select2;
+	char uname[50], pwd[50];
+
+	ptr = fopen("database.txt", "r");
+	printf("\t\tUser Sign In\n");
+	printf("Username: ");
+	fflush(stdin);
+	scanf("%[^\n]%*c", &uname);
+	printf("Password: ");
+	fflush(stdin);
+	scanf("%[^\n]%*c", &pwd);
+
+	while (fread(&b, recsize, 1, ptr) == 1)
+	{
+		if (strcmp(uname, b.uname) == 0 && strcmp(pwd, b.pwd) == 0)
+		{
+			system("cls");
+			printf("\t\tWelcome %s\n", b.uname);
+			printf("1\tReserve a train\n");
+			printf("2\tCancellation\n");
+			printf("3\tEdit travelling details\n");
+			printf("4\tView all available tickets\n");
+			printf("5\tBack\n\n");
+			printf("Enter your choice: ");
+			scanf("%d", &select2);
+			if (select2 == 1)
+				user1();
+			if (select2 == 2)
+				user2();
+			if (select2 == 3)
+				user3();
+			if (select2 == 4)
+				user4();
+			if (select2 == 5)
+				user();
+			flag = 1;
 			break;
 		}
-		break;
+		else{
+			;
+		}
 	}
-	printf("%s", name1);
+	if (flag == 0){
+		printf("Wrong username or password");
+		getch();
+		user();
+	}
 }
-read(){
+user1(){
+	printf("\t\tUser Reservation System");
+}
+user2(){
+	printf("Greed is good");
+}
+user3(){
+	printf("Foo bar");
+}
+user4(){
+	printf("Reanimate food");
+}
+user5(){
+	printf("Algorithm required");
+}
+
+//admin-related
+adminauthen(){
 	FILE *ptr;
-	ptr = fopen("Cprogram.txt", "r");
-	_getch;
-	fclose(ptr);
-	return main();
-}
-int main(){
-	int input1;
-tryagain:
-	printf("\tWelcome to the Train Reservation System.\n-----------------------------------------------------\nSelect whether you are User or Admin.\n1\tUser\n2\tAdmin\n3\tRead\nSelect your choice: ");
-	scanf_s("%d", &input1, 1);
+	int select;
+	char uname[50], pwd[50], c;
+
 	system("cls");
-	switch (input1){
-	case 1: user();
-		break;
-	case 2: admin();
-		break;
-	case 3: hidepass();
-		break;
-	default: printf("Invalid input. Please try again.");
-		goto tryagain;
+	printf("Admin credentials\n");
+	printf("========================================\n\n");
+	printf("Username: ");
+	fflush(stdin);
+	scanf("%[^\n]%*c", uname);
+	printf("Password: ");
+	char buffer[256] = { 0 };
+	int pos = 0;
+	do{
+		c = _getch();
+		if (isprint(c)){
+			buffer[pos++] = c;
+			printf("%c", '*');
+		}
+		else if ((c == '\b') && pos){
+			buffer[pos--] = '\0';
+			printf("%s", "\b \b");
+		}
+	} while (c != '\r');
+	strcpy(pwd, buffer);
+	if (strcmp(uname, "admin") == 0 && strcmp(pwd, "admin") == 0)
+		admin();
+	else{
+		printf("Wrong username or password. Press any key to return to main menu... ");
+		getch();
 	}
+}
+
+admin(){
+	int select;
+	system("cls");
+	printf("\t\tWelcome admin! \n");
+	printf("1\tNew Train Detail\n");
+	printf("2\tModify Train Detail\n");
+	printf("3\tCancel Train Detail\n");
+	printf("4\tView User Reservation\n");
+	printf("5\tView User Cancellation\n");
+	printf("6\tView Username List\n");
+	printf("7\tCancel\n\n");
+	printf("Enter your choice: ");
+	scanf("%d", &select);
+	if (select == 1)
+		newdetail();
+	else if (select == 2)
+		moddetail();
+	else if (select == 3)
+		cancdetail();
+	else if (select == 4)
+		viewreserv();
+	else if (select == 5)
+		viewcanc();
+	else if (select == 6)
+		viewlist();
+	else if (select == 7)
+		main();
+	else{
+		printf("Invalid choice. Please try again");
+		printf("Press any key to continue... ");
+		getch();
+		admin();
+	}
+}
+newdetail(){
+	printf("Invalid choice11. Please try again");
+}
+moddetail(){
+	printf("Invalid choice22. Please try again");
+}
+cancdetail(){
+	printf("Invalid choice333. Please try again");
+}
+viewreserv(){
+	printf("Invalid choice44. Please try again");
+}
+viewcanc(){
+	printf("Invalid choice55. Please try again");
+}
+viewlist(){
+	FILE *ptr;
+	
+	system("cls");
+	ptr = fopen("database.txt", "r");
+	printf("================================================================================\n"); 
+	printf("List of usernames\t\tPasswords\n");  
+	printf("================================================================================"); 
+	while (fread(&b, recsize, 1, ptr) == 1) {
+		printf("\n%s", b.uname);
+		printf("\t\t%s\n", b.pwd);
+	}
+	printf("\n\nPress any key to return to admin menu... ");
+	getch();
+	admin();
 }
